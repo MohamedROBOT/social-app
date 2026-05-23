55c5ee4a6a30c5a97706f2fbcc97b1629848af43
MohamedROBOT <muhammetahmed12@gmail.com>
2026-05-23 22:56:27 +0300

    ignore firebase cred

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -58,7 +58,7 @@ class CommentService {
     return comments;
   }
 async getOne (commentId: Types.ObjectId){
-    //nested populate 
+    //nested populate
     return await this.commentRepository.getOne({_id: commentId},{},{populate:[{path:"userId"},{path:"postId", populate:{path:"userId"}}]})
 }
   async delete(id: Types.ObjectId, userId: Types.ObjectId) {
